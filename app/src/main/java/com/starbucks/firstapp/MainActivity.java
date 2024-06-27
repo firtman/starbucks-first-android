5b6310580cdd7c249fc827e7676e575207693ca9
Maximiliano Firtman <firtman@gmail.com>
2024-06-27 11:21:17 -0400

    Landscape variants

--- app/src/main/java/com/starbucks/firstapp/MainActivity.java
+++ app/src/main/java/com/starbucks/firstapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.starbucks.firstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -34,11 +36,18 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         Log.d("Lifecycle", "onCreate");
-
         setContentView(R.layout.activity_main);
 
+        EditText editUser = findViewById(R.id.edit_user);
+        String username = editUser.getText().toString();
+
+        findViewById(R.id.btn_login).setOnClickListener(view -> {
+            Intent intent = new Intent(this, NextActivity.class);
+            startActivity(intent);
+        });
 
         //tinyurl.com/starbucks-workshop
 
---
+++ app/src/main/java/com/starbucks/firstapp/NextActivity.java
@@ -0,0 +1,18 @@
+package com.starbucks.firstapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+    }
+}
